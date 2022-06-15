aed6033bc4cfb3e7032d9d24561d495be127463e
Ed-dev <lordblackprogrammer@outlook.com>
2022-06-15 00:45:51 -0300

    Pequeno ajuste

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import { GlobalStyle, Theme } from './global';
 import { ThemeProvider } from 'styled-components';
-import { store } from './redux/store';
 
 import { HomePage } from './pages/homepage';
-import { Provider } from 'react-redux';
 
 const App = () => {
   return (
-    <Provider store={store}>
+    <>
       <ThemeProvider theme={Theme}>
         <HomePage />
       </ThemeProvider>
       <GlobalStyle />
-    </Provider>
+    </>
   );
 };
 
